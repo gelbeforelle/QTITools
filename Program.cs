28de1685c48988d9bd7ba9831fab1d0ba3f18401
gelbeforelle <niclas.k1656@gmail.com>
2021-09-09 11:51:18 +0200

    Add simple storage class for tasks

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+
+namespace QTITools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new QTIParser("C:/Users/Niclas/Documents/QTI/test.zip");
+            foreach(ZipArchiveEntry entry in file){ 
+                if(file.ReadEntry(entry).Tasks != null) foreach(ITask t in file.ReadEntry(entry).Tasks){
+                   if(entry != null) Console.WriteLine(t.ToLia());
+                   else Console.WriteLine("NullEntry");
+                }
+                    else Console.WriteLine("NullTasks");
+                }
+            Console.WriteLine("Done.");
+        }
+    }
+}
