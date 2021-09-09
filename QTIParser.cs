64464c812a2bd66fdf82f029b568c7c74c18068e
gelbeforelle <niclas.k1656@gmail.com>
2021-09-09 20:09:31 +0200

    Deactivate debug output lines

--- QTIParser.cs
+++ QTIParser.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.IO;
 using System.IO.Compression;
 using System.Xml;
-
+using System.Collections.Generic;
 
 partial class QTIParser : IEnumerable{
     public QTIParser(string path){
@@ -11,21 +11,72 @@ partial class QTIParser : IEnumerable{
         // ZipArchiveEntry entry = Source.GetEntry(bodyPath);
     }
 
-    public string ReadEntry(ZipArchiveEntry entry){
+    public TaskFile ReadEntry(ZipArchiveEntry entry){
         //if(path == blacklist) return null;
         //ZipArchiveEntry entry = Source.GetEntry(path);
+        var result = new TaskFile();
         reader = XmlReader.Create(entry.Open());
+        var corrects = new List<string>();
+        //var responses = new Dictionary<string, string[]>();
         while(!(reader.NodeType == XmlNodeType.EndElement) || reader.Name == "p" || reader.Name == "simpleChoice"){
             reader.Read();
+            if(reader.Name == "responseDeclaration" && reader.NodeType == XmlNodeType.Element){
+                //var key = reader.GetAttribute(0); //Should use pattern-maching!!!
+                
+                while(!(reader.Name == "responseDeclaration" && reader.NodeType == XmlNodeType.EndElement || reader.Name == "itemBody")){
+                    reader.Read();
+                    if(reader.NodeType == XmlNodeType.Text) corrects.Add("identifier=\""+reader.Value+"\"");
+                }
+                
+                //responses.Add(key, content.ToArray());    
+            }
             //Console.WriteLine(reader.Name);
-            if(XmlNodeType.Text == reader.NodeType) Console.WriteLine(reader.Value);
+            if(reader.Name == "assessmentItem"){
+                
+                while(reader.Name == "p" || reader.NodeType == XmlNodeType.Text){
+                    reader.Read();
+                    if(reader.NodeType == XmlNodeType.Text) result.AddText(reader.Value); 
+                }
+            } // ???
+                if(reader.Name == "choiceInteraction"){
+                    bool isMC = true;
+                    List<string> answers = new List<string>();
+                    List<bool> answerKey = new List<bool>();
+                    for(int i=0;i<reader.AttributeCount;i++){
+                        if(reader.GetAttribute(i) == "maxChoices=\"1\"") isMC = false;
+                    }
+
+                    while(!(reader.NodeType == XmlNodeType.EndElement && reader.Name == "choiceInteraction")){
+                        string choice = string.Empty;
+                        while(!(reader.NodeType == XmlNodeType.EndElement && reader.Name == "simpleChoice")){
+                            if(reader.Name == "simpleChoice"){
+                                if(corrects.Contains(reader.GetAttribute(0))) answerKey.Add(true);
+                                    else answerKey.Add(false);
+                            }
+                            reader.Read();
+                            if(reader.NodeType == XmlNodeType.Text) choice += reader.Value + "\n";
+                        }
+                        answers.Add(choice);
+                        reader.Read();
+                    }
+                    if(isMC) result.AddTask(new MultipleChoice(answers.ToArray(), answerKey.ToArray()));
+                        else{
+                            int correctIndex = 0;
+                            correctIndex = answerKey.FindIndex(x => x);
+                            result.AddTask(new SingleChoice(answers.ToArray(), correctIndex));
+                        }
+                    
+                }
+            //}
+           // if(XmlNodeType.Text == reader.NodeType) Console.WriteLine(reader.Value);
+          //  Console.WriteLine(reader.Name);
         }
-        return null;
+        return result;
     }
 
     private XmlReader reader;
     private ZipArchive Source;
-    public const string blacklist = "imsmanifest.xml";
+    //public const string blacklist = "imsmanifest.xml";
 
     
 
